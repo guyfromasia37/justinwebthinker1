f0d5eee6299af2d9f4a1b58f9e500287cc70c5ef
guyfromasia37 <justinahx10@gmail.com>
2026-01-18 09:20:26 +0000

    justin

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,59 @@
 print("Hello from lesson 2")
-
+# print("red")
+# print("orange")
+# print("yellow")
+# print("green")
+print("blue")
+# print("indigo")
+# print("violet")
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+
+
+# repeat 10 times
+#   say hey!
+#   move 10 steps
+
+
+
+
+# ask what is your age
+    # if answer is less than 18 say excess denied
+    # if else say welcome!
+
+
+
+
+#create vairable for glass,plastic and paper
+#create a repeat loop until there is no more items
+    # if the item is glass
+        # put the item in the glass bin
+    #else if item is plastic
+        #put in plastic bin
+    #else if item is paper
+        #put item in paper bin
+    #else 
+        #throw away
+
+
+
+
+
+    #ask what is the password
+        #if answer is tuff5437 say welcome back!
+        #if else say wrong password you hacker!
+
+
+
+
+
+    #make a vairable for test 1,2 and 3
+        #test 1 = test1 score x 20%
+        #test 2 = test2 score x 40%
+        #test 3 = test3 score x 40%
+        #make vairable finalscore
+        #finalscore= test1+test2+test3
