ba268f05d6a857e060043c0521a2290702093bfb
guyfromasia37 <justinahx10@gmail.com>
2026-05-03 08:49:27 +0000

    q2

--- CT06_endsem/q2.py
+++ CT06_endsem/q2.py
@@ -8,46 +8,46 @@ The program must perform several operations on this list.
 Program Requirements:
 - Use the list:
   planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus"]
-- Print the 3rd item using index
-- Append "neptune" to the list
-- Rename "mars" to "muskworld"
-- Remove "uranus" from the list
-- Using a for loop, print all the planets one by one
+  - Print the 3rd item using index
+  - Append "neptune" to the list
+  - Rename "mars" to "muskworld"
+  - Remove "uranus" from the list
+  - Using a for loop, print all the planets one by one
+  
+  ============================================================
+  """
+  
+  # ============================================================
+  # Step 1: Create the list
+  # ============================================================
+  
+planets=["mercury","venus","earth","mars","jupiter","saturn","uranus"]
+  
+  # ============================================================
+  # Step 2: Print the 3rd item (Test Case 1)
+  #     - Comment after testing
+  # ============================================================
+  
+print(planets[3])
+  
+  # ============================================================
+  # Step 3: Append "neptune"
+  # ============================================================
 
-============================================================
-"""
-
-# ============================================================
-# Step 1: Create the list
-# ============================================================
-
-
-
-# ============================================================
-# Step 2: Print the 3rd item (Test Case 1)
-#     - Comment after testing
-# ============================================================
-
-
-
-# ============================================================
-# Step 3: Append "neptune"
-# ============================================================
-
-
-
-# ============================================================
-# Step 4: Rename "mars" to "muskworld"
-# ============================================================
-
-
-
-# ============================================================
-# Step 5: Remove "uranus"
-# ============================================================
-
-
-
-# ============================================================
-# Step 6: Loop through and print all planets
-# ============================================================
+planets.append("neptune")
+  
+  # ============================================================
+  # Step 4: Rename "mars" to "muskworld"
+  # ============================================================
+  
+planets[3] = "muskworld"  
+  # ============================================================
+  # Step 5: Remove "uranus"
+  # ============================================================
+  
+planets.remove("uranus")
+  
+  # ============================================================
+  # Step 6: Loop through and print all planets
+  # ============================================================
+print(planets)
